57d820fbc72ae6d712a7c5d2c4e7dac50d8a6748
vanloi1705 <daovanloi1705@gmail.com>
2025-12-16 01:33:39 +0700

    Fix: Sua loi chinh ta

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'BÀI KIỂM TRA FLUTTER – VĂN LỢI',
+      title: 'BÀI KIỂM TRA FLUTTER – VĂN LỢIzzz',
       home: HomePage(),
     );
   }
